e25e35841a16764faf41e6545b0d6f86be375b97
Sakshi Choudhary <97943236+saks-hi29@users.noreply.github.com>
2024-08-29 10:49:42 +0530

    Add files via upload

---
+++ belly.java
@@ -0,0 +1,16 @@
+package com.verizon.mavencucumberapp1;
+
+public class belly {
+	public void eat(int cukes) {
+		System.out.println("step started");
+	}
+	
+	public void waitStep() {
+		System.out.println("waiting after step1");
+	}
+	
+	public void endProcess() {
+		System.out.println("all steps are completed");
+	}
+
+}
